8daee8790dbe21ffe9f86b54eb9a589a6c7e9c19
Charlotte Cleary <CCleary2@uclan.ac.uk>
2024-07-24 14:06:35 +0100

    CVector2D class: Added default constructor and constructor with parameters. Changed X and Y access to private, so added public setter and getter functions. Overloaded * operator for scalar multiplication. Added DotProduct() function. Added Normalise() function.

--- CVector2D.cpp
+++ CVector2D.cpp
@@ -1,6 +1,19 @@
 #include "CVector2D.h"
 #include "math.h" // sqrt()
 
+CVector2D::CVector2D() :
+    // Defualt values of X = 0.0f, Y = 0.0f
+    X(0.0f),
+    Y(0.0f)
+{
+}
+
+CVector2D::CVector2D(const float& NewX, const float& NewY) :
+    X(NewX),
+    Y(NewY)
+{
+}
+
 float CVector2D::Magnitude() const
 {
     return sqrt(X*X + Y*Y);
@@ -25,6 +38,31 @@ CVector2D CVector2D::operator-(CVector2D const& Vec)
     CVector2D Res;
     Res.X = X - Vec.X;
     Res.Y = Y - Vec.Y;
+
+    return Res;
+}
+
+CVector2D CVector2D::operator*(float const& K)
+{
+    CVector2D Res;
+    Res.X = X * K;
+    Res.Y = Y * K;
+    
+    return Res;
+}
+
+float CVector2D::DotProduct(const CVector2D& OtherVec) const
+{   
+    return (X * OtherVec.GetX() + Y * OtherVec.GetY()) ;
+}
+
+CVector2D CVector2D::Normalise() const
+{
+    float Length = Magnitude();
+
+    CVector2D Res;
+    Res.SetX(X / Length);
+    Res.SetY(Y / Length);
 
     return Res;
 }
--- CVector2D.h
+++ CVector2D.h
@@ -2,15 +2,36 @@
 
 class CVector2D
 {
-public:
-	float X;
-	float Y;
+private:
+	float X; // = 0.0f using defualt constructor
+	float Y; // = 0.0f using default constructor
 
 public:
+	// Constructors
+	CVector2D(); // Default constructor
+	CVector2D(const float&, const float&);
+
+	// Destructors
+
+	// Getters
+	float GetX() const { return X; }
+	float GetY() const { return Y; }
+
+	// Setters
+	void SetX(const float NewX) { X = NewX; }
+	void SetY(const float NewY) { Y = NewY; }
+
+	// Magnitude functions
 	float Magnitude() const;
 	float SquareMagnitude() const;
 
+	// Operators for adding, subtracting, scalar multiplication
 	CVector2D operator+(CVector2D const& Vec);
 	CVector2D operator-(CVector2D const& Vec);
+	CVector2D operator*(float const& K);
+
+	// Dot product function
+	float DotProduct(const CVector2D&) const;
+	CVector2D Normalise() const;
 };
 
