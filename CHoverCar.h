2027ebcd34b2a16da7ae9f63b1415ba66bed62cb
Charlotte Cleary <CCleary2@uclan.ac.uk>
2024-07-25 20:51:22 +0100

    Commented out problematic code.

--- CHoverCar.h
+++ CHoverCar.h
@@ -8,41 +8,41 @@ using namespace tle;
 
 class CHoverCar
 {
-protected:
-	Model* mDummyModel;
-	Model* mModel;
-	CVector2D mInitalPos;
-	float mInitialRotation;
-	CVector2D mFacingVec;
-	CVector2D mMomentumVec;
-	int mHealth;
-	float mRotationSpeed;
-	std::vector <SWaypoint> mWaypointsVec;
-	int mWaypointCounter;
-	int mCheckpointCounter;
-	int mLapCounter;
-
-	// protected methods
-	void CheckCurrentWaypoint();
-	void MoveToInitialPosRot();
-
-public:
-	// constructors
-	CHoverCar();
-	CHoverCar(Mesh*, Mesh*, const CVector2D&, const float&);
-
-	// destructor
-	~CHoverCar();
-
-	// getters
-	Model* GetDummyModel() const { return mDummyModel; };
-	Model* GetModel() const { return mModel; };
-	CVector2D GetMomentumVec() const { return mMomentumVec; };
-	int GetHealth() const { return mHealth; };
-	int GetCheckpointCounter() const { return mCheckpointCounter; };
-	int GetLapCounter() const { return mLapCounter; };
-
-	// other public methods
-	void SetWaypointsVec(const std::vector <SWaypoint>&);
+//protected:
+//	Model* mDummyModel;
+//	Model* mModel;
+//	CVector2D mInitalPos;
+//	float mInitialRotation;
+//	CVector2D mFacingVec;
+//	CVector2D mMomentumVec;
+//	int mHealth;
+//	float mRotationSpeed;
+//	std::vector <SWaypoint> mWaypointsVec;
+//	int mWaypointCounter;
+//	int mCheckpointCounter;
+//	int mLapCounter;
+//
+//	// protected methods
+//	void CheckCurrentWaypoint();
+//	void MoveToInitialPosRot();
+//
+//public:
+//	// constructors
+//	CHoverCar();
+//	CHoverCar(Mesh*, Mesh*, const CVector2D&, const float&);
+//
+//	// destructor
+//	~CHoverCar();
+//
+//	// getters
+//	Model* GetDummyModel() const { return mDummyModel; };
+//	Model* GetModel() const { return mModel; };
+//	CVector2D GetMomentumVec() const { return mMomentumVec; };
+//	int GetHealth() const { return mHealth; };
+//	int GetCheckpointCounter() const { return mCheckpointCounter; };
+//	int GetLapCounter() const { return mLapCounter; };
+//
+//	// other public methods
+//	void SetWaypointsVec(const std::vector <SWaypoint>&);
 };
 
