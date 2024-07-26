5f3e4971799c246a44bc2390f4c70cda6623781a
Charlotte Cleary <CCleary2@uclan.ac.uk>
2024-07-26 16:55:56 +0100

    Added CWallSection_LI class.

---
+++ CWallSection_LI.cpp
@@ -0,0 +1,61 @@
+#include "CWallSection_LI.h"
+
+// Defualt constructor - dont want to use, does nothing
+CWallSection_LI::CWallSection_LI()
+{
+}
+
+CWallSection_LI::CWallSection_LI(Mesh* DummyMesh, Mesh* WallMesh, Mesh* IsleMesh, const CVector2D& Pos, const float& Rot)  :
+	CLevelItem(Pos, Rot)
+{
+	// Create dummy model
+	DummyModel = DummyMesh->CreateModel();
+
+	// Loop through walls array
+	for (int i = 0; i < NumWalls; i++)
+	{
+		// Create wall models and attach to dummy
+		WallsArr[i] = WallMesh->CreateModel();
+		WallsArr[i]->AttachToParent(DummyModel);
+
+		// Move wall models relative Z pos by offset
+		if (i == 0)
+		{
+			WallsArr[i]->MoveLocalZ(-WallOffset);
+		}
+		else
+		{
+			WallsArr[i]->MoveLocalZ(WallOffset);
+		}
+	}
+	
+	// Loop through isles array
+	for (int i = 0; i < NumIsles; i++)
+	{
+		// Create isle models and attach to dummy
+		IslesArr[i] = IsleMesh->CreateModel();
+		IslesArr[i]->AttachToParent(DummyModel);
+
+		// Move isle models relative Z pos by offset
+		if (i == 0)
+		{
+			IslesArr[i]->MoveLocalZ(-IsleOffset);
+		}
+		else if (i == 2)
+		{
+			IslesArr[i]->MoveLocalZ(IsleOffset);
+		}
+	}
+
+	// Move dummy model to pos
+	DummyModel->SetX(PosXZ.GetX());
+	DummyModel->SetZ(PosXZ.GetY());
+
+	// Rotate dummy model by rotation
+	DummyModel->RotateY(RotationY);
+}
+
+// Does nothing
+CWallSection_LI::~CWallSection_LI()
+{
+}
---
+++ CWallSection_LI.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <TL-Engine11.h>
+#include "CLevelItem.h"
+#include "CCollider_Box.h"
+
+using namespace tle;
+
+const float WallOffset = 8.0f;
+const float IsleOffset = 16.0f;
+const int NumIsles = 3;
+const int NumWalls = 2;
+const float WallColliderWidth = 5.0f;
+const float WallColliderDepth = 38.3f;
+
+class CWallSection_LI :
+    public CLevelItem
+{
+private:
+    Model* DummyModel;
+    Model* IslesArr[NumIsles];
+    Model* WallsArr[NumWalls];
+    CCollider_Box BoxCollider;
+
+public:
+    // constructors
+    CWallSection_LI();
+    CWallSection_LI(Mesh* DummyMesh, Mesh* WallMesh, Mesh* IsleMesh, const CVector2D& Pos, const float& Rot);
+
+    // destructor
+    ~CWallSection_LI();
+
+    // setters
+
+    // getters
+};
+
