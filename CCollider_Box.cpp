04da179fc114293ae41d182d90aa32aeb72e9ac9
Charlotte Cleary <CCleary2@uclan.ac.uk>
2024-08-16 16:02:01 +0100

    Added corner array and updated constructors.

--- CCollider_Box.cpp
+++ CCollider_Box.cpp
@@ -3,19 +3,42 @@
 // Default constructor
 CCollider_Box::CCollider_Box()
 {
+	for (int i = 0; i < NumBoxCorners; i++)
+	{
+		CornersArray[i] = nullptr;
+	}
 }
 
 // Constructor with WidthDepth passed in
-CCollider_Box::CCollider_Box(const CVector2D& NewWD) :
-	WidthDepth(NewWD)
-{
-}
-
-// Constructor with WidthDepth and position passed in
-CCollider_Box::CCollider_Box(const CVector2D& NewWD, const CVector2D& NewPos) :
+CCollider_Box::CCollider_Box(const CVector2D& NewWD, Mesh* DummyMesh, Model* BaseObject) :
 	WidthDepth(NewWD),
-	CCollider(NewPos)
+	CCollider(DummyMesh, BaseObject)
 {
+	// Calculate half width and depth.
+	const float HalfWidth = 0.5f * NewWD.GetX();
+	const float HalfDepth = 0.5f * NewWD.GetY();
+	
+	for (int i = 0; i < NumBoxCorners; i++)
+	{
+		// Create corner dummy models, attach to central dummy model, then move to correct local position.
+		CornersArray[i] = DummyMesh->CreateModel();
+		CornersArray[i]->AttachToParent(ColliderCentre);
+
+		// 0 = top left; 1 = top right; 2 = bottom left; 3 = bottom right
+	}
+
+	// 0 = top left; 1 = top right; 2 = bottom left; 3 = bottom right
+	CornersArray[0]->SetLocalX(-HalfWidth);
+	CornersArray[0]->SetLocalZ(HalfDepth);
+
+	CornersArray[1]->SetLocalX(HalfWidth);
+	CornersArray[1]->SetLocalZ(HalfDepth);
+
+	CornersArray[2]->SetLocalX(-HalfWidth);
+	CornersArray[2]->SetLocalZ(-HalfDepth);
+
+	CornersArray[3]->SetLocalX(HalfWidth);
+	CornersArray[3]->SetLocalZ(-HalfDepth);
 }
 
 // Destructor
--- CCollider_Box.h
+++ CCollider_Box.h
@@ -3,17 +3,19 @@
 #include "CCollider.h"
 #include "CVector2D.h"
 
+const int NumBoxCorners = 4;
+
 class CCollider_Box :
     public CCollider
 {
 private:
     CVector2D WidthDepth;
+    Model* CornersArray[NumBoxCorners]; // 0 = top left; 1 = top right; 2 = bottom left; 3 = bottom right
 
 public:
     // Constructors
     CCollider_Box();
-    CCollider_Box(const CVector2D&);
-    CCollider_Box(const CVector2D&, const CVector2D&);
+    CCollider_Box(const CVector2D& NewWidthDepth, Mesh* DummyMesh, Model* BaseObject);
 
     // Destructor
     ~CCollider_Box();
